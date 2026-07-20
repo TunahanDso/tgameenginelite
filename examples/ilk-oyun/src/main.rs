53bdacbbec1452c4abb558f5073cdf69ac554e16
TunahanDso <89835977+TunahanDso@users.noreply.github.com>
2026-07-20 22:03:13 +0300

    İlk oyuna Türkçe girdi ve zaman örneği ekle

--- examples/ilk-oyun/src/main.rs
+++ examples/ilk-oyun/src/main.rs
@@ -1,9 +1,24 @@
-use tgame::onsoz::{Oyun, OyunSonucu, Sahne};
+use tgame::onsoz::{Oyun, OyunAkisi, OyunSonucu, Sahne, Tus};
 
 fn main() -> OyunSonucu {
     Oyun::yeni("İlk Tgame Oyunum")
         .cozunurluk(800, 600)
         .mod_klasoru("modlar")
         .sahne_ekle(Sahne::yeni("Başlangıç"))
+        .her_kare(|girdi, zaman| {
+            if girdi.bu_kare_basildi_mi(Tus::Bosluk) {
+                println!(
+                    "Boşluk tuşuna {}. karede, {:.3} saniyede basıldı.",
+                    zaman.kare_sayisi(),
+                    zaman.toplam_saniye(),
+                );
+            }
+
+            if girdi.bu_kare_basildi_mi(Tus::Kacis) {
+                OyunAkisi::Kapat
+            } else {
+                OyunAkisi::DevamEt
+            }
+        })
         .calistir()
 }
